87f6bc7f4d8f830740bf9a6dfd4ea20eeb6c1fde
sisasam <34127885+sisasam@users.noreply.github.com>
2018-05-06 22:36:07 +0200

    Hat nichts gebracht

--- adsaufgabe1/src/adsaufgabe1/Tiefensuche_MeinName.java
+++ adsaufgabe1/src/adsaufgabe1/Tiefensuche_MeinName.java
@@ -1,18 +1,22 @@
 package adsaufgabe1;
 
 public class Tiefensuche_MeinName implements ITiefensuche {
+	
+	private boolean bekannt;
+	private boolean fertig;
+	private int vorgänger;
 
 	public Tiefensuche_MeinName() {
-		for (int i = 0; i < Graph.anfrageKnotenanzahl(); i++) {
-			
-		}
+		
 	}
 
 	@Override
 	public Kantentyp kantenStatus(IGraph graph, 
 			int indexQuelleDerKante, int indexZielDerKante) {
-		
-		return null;
+		for (int i = 0; i < graph.anfrageKnotenanzahl(); i++) {
+			istBekannt(i) 
+		}
 	}
+	
 
 }
